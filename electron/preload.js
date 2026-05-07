f4389be9fc3f8a6b06a9c95bd1cf31a394f4e5f8
Developer <developer@example.com>
2026-05-07 09:45:20 +0800

    Release v5.2.3 - 修复 preload.js 模块格式，更新版本号

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from 'electron';
+const { contextBridge, ipcRenderer } = require('electron');
 
 // 向渲染进程暴露安全的 API
 contextBridge.exposeInMainWorld('electronAPI', {
